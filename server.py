90c58a04b0c9bf4ea6d5254717e15d1a8bd6d023
Arsh Chauhan <achauhan@alaska.edu>
2016-11-30 10:52:25 -0900

    Parse file every 5 minutes.Added CLI args

--- server.py
+++ server.py
@@ -2,19 +2,30 @@
 
 # lserver.py: Web server for log visualization project 
 # 11/29/2016
-# Last Edited: 11/29/2016
+# Last Edited: 11/30/2016
 
 import SimpleHTTPServer
 import BaseHTTPServer
 import json
+import threading
+import argparse
+
 
 import log_parser
 
-
+#FIXME: Clean up timer on KeyboardInterrupt 
+def parse_auth_log(seconds):
+		#ip_addresses, invalid_users = log_parser.parse_auth_log("/var/log/auth.log")
+		ip_addresses, invalid_users = log_parser.parse_auth_log("logs/auth.log")
+		with open('auth_log_ips','w') as ip_file:
+			ip_file.write(json.dumps(ip_addresses))
+		ip_file.closed
+		self_timer = threading.Timer(seconds,parse_auth_log)
+		self_timer.start()
+	
 class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
-	def do_GET(self):
-		print("Path is:"+self.path)
 
+	def do_GET(self):
 		if self.path =='/':
 			with open ('jmap.html') as index:
 				self.send_response(200)
@@ -26,10 +37,11 @@ class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			self.send_response(200)
 			self.send_header('Content-type','text/html')
 			self.end_headers()
-			#ip_addresses, invalid_users = log_parser.parse_auth_log("/var/log/auth.log")
-			ip_addresses, invalid_users = log_parser.parse_auth_log("logs/auth.log")
-			self.wfile.write(json.dumps(ip_addresses))
-			self.wfile.close()
+			with open('auth_log_ips','r') as ip_file:
+				ip_addresses = json.loads(ip_file.read())
+				self.wfile.write(json.dumps(ip_addresses))
+				self.wfile.close()
+			ip_file.closed
 		else:
 			server_log = open('logs/server.log','a')
 			server_log.write("Invalid URL visited: "+self.path+"\n");
@@ -41,6 +53,7 @@ class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
 							 <img src="http://thecatapi.com/api/images/get?format=src&type=gif"\>\
 							 <br> </h1><a href="/">Home Page</a> </body></html>')
 			self.wfile.close()
+			server_log.close()
 
 
 	def do_POST(self):
@@ -50,7 +63,13 @@ class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
 		self.wfile.write("This server does not support POST yet")
 
 if __name__ == '__main__':
+	cli_args = argparse.ArgumentParser(description='Parse common *nix logs and map on a webpage')
+	cli_args.add_argument('--timer',dest='timer_sec',type=int,help='Parse log file every --timer seconds',default=300)
+	cli_args.add_argument('--port',dest='port',type=int,help='Port to run this server',default=8080)
+	args = cli_args.parse_args()
+
+	parse_auth_log(args.timer_sec)
 	handler = web_logger_handler
-	serverAddress = ('',8080)
+	serverAddress = ('',args.port)
 	server = BaseHTTPServer.HTTPServer(serverAddress,handler)
 	server.serve_forever()
