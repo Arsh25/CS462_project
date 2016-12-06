5f45885c8ac1f406c6bbbc1be1ee53179831846d
Arsh Chauhan <achauhan@alaska.edu>
2016-12-05 16:10:52 -0900

    ONly send unique IP's to server for plotting. Do not erase auth_log_ips every parse(Prevents us from loosing data due to log rotation)

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
 
 # lserver.py: Web server for log visualization project 
-# 11/29/2016
-# Last Edited: 11/30/2016
+# Arsh Chauhan 
+#11/29/2016
+# Last Edited: 12/5/2016
 
 import SimpleHTTPServer
 import BaseHTTPServer
@@ -15,16 +16,17 @@ from datetime import datetime
 import log_parser
 
 #FIXME: Clean up timer on KeyboardInterrupt 
-def parse_auth_log(seconds):
+def parse_auth_log(log_file,seconds):
 		#ip_addresses, invalid_users = log_parser.parse_auth_log("/var/log/auth.log")
-		ip_addresses, invalid_users = log_parser.parse_auth_log("logs/auth.log")
-		with open('auth_log_ips','w') as ip_file:
-			ip_file.write(json.dumps(ip_addresses))
+		ip_addresses, invalid_users = log_parser.parse_auth_log(log_file)
+		with open('auth_log_ips','a') as ip_file:
+			for ip in ip_addresses:
+				ip_file.write(ip+'\n')			
 		ip_file.closed
 		with open('logs/update.log','a') as parser_log:
 			parser_log.write("Ran parse_auth_log at: " + str(datetime.now())+'\n')
 		parser_log.closed
-		self_timer = threading.Timer(seconds,parse_auth_log,args=[seconds])
+		self_timer = threading.Timer(seconds,parse_auth_log,args=[log_file,seconds])
 		self_timer.start()
 	
 class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -42,8 +44,10 @@ class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			self.send_header('Content-type','text/html')
 			self.end_headers()
 			with open('auth_log_ips','r') as ip_file:
-				ip_addresses = json.loads(ip_file.read())
-				self.wfile.write(json.dumps(ip_addresses))
+				ip_addresses = ip_file.read().splitlines()
+				unique_ips = set(ip_addresses) #Convert list to set to get unique IP's
+				response = list(unique_ips) # Convert unique IP's set back to list
+				self.wfile.write(json.dumps(response))
 				self.wfile.close()
 			ip_file.closed
 		else:
@@ -70,9 +74,10 @@ if __name__ == '__main__':
 	cli_args = argparse.ArgumentParser(description='Parse common *nix logs and map on a webpage')
 	cli_args.add_argument('--timer',dest='timer_sec',type=int,help='Parse log file every --timer seconds',default=300)
 	cli_args.add_argument('--port',dest='port',type=int,help='Port to run this server',default=8080)
+	cli_args.add_argument('--log',dest='log_file',type=str,help='File to parse as auth.log',default='logs/auth.log')
 	args = cli_args.parse_args()
 
-	parse_auth_log(args.timer_sec)
+	parse_auth_log(args.log_file,args.timer_sec)
 	handler = web_logger_handler
 	serverAddress = ('',args.port)
 	server = BaseHTTPServer.HTTPServer(serverAddress,handler)
