a2953d57e4242ee2feefd248e4c7650afbf3c4e2
Arsh Chauhan <achauhan@alaska.edu>
2016-12-06 16:52:31 -0900

    Was not sendingresponsecode for /live

--- server.py
+++ server.py
@@ -75,6 +75,7 @@ class web_logger_handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			response = []
 			for entry in rows:
 					response.append({'ip':entry['ip_address'],'lat':entry['lat'],'long':entry['long']})
+			self.send_response(200)
 			self.send_header('Content-type','application/json')
 			self.end_headers()
 			self.wfile.write(json.dumps(response))
