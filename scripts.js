fc0c611f2c711df7f2a48b7c67c726d6e5738de7
Jason Warta <jasonwarta@gmail.com>
2016-12-07 17:59:59 -0900

    removed old and unused code

--- scripts.js
+++ scripts.js
@@ -90,56 +90,10 @@ function getDataFromServer(){
                 attack = data[i];
                 msg = attack['ip']
                 addMarkerToMap(attack["lat"],attack["long"],msg);
-                ipCount[attack["ip"]]++;
             }
-            populateTables();
+            // populateTables();
         }
     }
     xmlHttp.open("GET","live",true);
     xmlHttp.send();
 }
-
-// function httpGetAsync(theUrl,ip){
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function(){
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-//             var json = xmlHttp.responseText;
-//             var obj = JSON.parse(json);
-//             addMarkerToMap(obj['latitude'],obj['longitude'],ip);
-//         }
-//     }
-//     xmlHttp.open("GET", theUrl, true);
-//     xmlHttp.send(null);
-// }
-
-// function parseIPs(){
-//     var ip_pattern = new RegExp("(?:[0-9]{1,3}\.){3}[0-9]{1,3}$");
-//     var textArea = document.getElementById('ip-entry').value;
-//     var lines = textArea.split('\n');
-//     for (var i = 0; i < lines.length; i++){
-//         if (ip_pattern.test(lines[i])){
-//             httpGetAsync("https://freegeoip.net/json/"+lines[i],lines[i])
-//         } else {
-//             console.log("Didn't recognize "+lines[i]+" as a valid IP address.")
-//         }
-//     }
-// }
-
-// function getDataFromServer()
-// {
-//     var req = new XMLHttpRequest();
-//     req.onreadystatechange = function()
-//     {
-//         if (req.readyState == 4 && req.status == 200)
-//         {
-//             var ip_addresses = JSON.parse(req.responseText);
-//             for (var i=0;i<ip_addresses.length;i++)
-//             {   
-//                 ip_addresses[i] = ip_addresses[i].trim();
-//                 document.getElementById('ip-entry').value += '\n'+ip_addresses[i];
-//             }
-//         }
-//     }
-//     req.open("GET","live",true);
-//     req.send(null);
-// }
